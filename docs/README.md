3970803c5768e1312411eb929301ed86320ff51f
Kaile <kailechong060@gmail.com>
2026-08-17 13:19:29 +0800

    Changed Duke.java to Drax.java, added greeting for chatbot, and added information to README.md

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Drax User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Drax.java
@@ -0,0 +1,16 @@
+public class Drax {
+    public static void main(String[] args) {
+        String banner = """
+                ██████╗ ██████╗  █████╗ ██╗  ██╗
+                ██╔══██╗██╔══██╗██╔══██╗╚██╗██╔╝
+                ██║  ██║██████╔╝███████║ ╚███╔╝
+                ██║  ██║██╔══██╗██╔══██║ ██╔██╗
+                ██████╔╝██║  ██║██║  ██║██╔╝ ██╗
+                ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+                """;
+        System.out.println(banner);
+        System.out.println("Infinite Salutations! I'm Drax!.");
+        System.out.println("What's on your mind today?");
+        System.out.println("Goodbye. Hope to see you again soon!");
+    }
+}

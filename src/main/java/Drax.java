6b31072e3456c9cecbca41dc351d18e95b02b52f
Kaile <kailechong060@gmail.com>
2026-08-19 12:27:43 +0800

    Added echo functionality for chatbot

--- src/main/java/Drax.java
+++ src/main/java/Drax.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Drax {
     public static void main(String[] args) {
         String banner = """
@@ -9,8 +11,20 @@ public class Drax {
                 ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
                 """;
         System.out.println(banner);
-        System.out.println("Infinite Salutations! I'm Drax!.");
+        System.out.println("Infinite Salutations! I'm Drax!");
         System.out.println("What's on your mind today?");
-        System.out.println("Goodbye. Hope to see you again soon!");
+        Scanner reader = new Scanner(System.in);
+        String n;
+        while(reader.hasNextLine()) {
+            n = reader.nextLine();
+            if (n.equals("bye")) {
+                System.out.println("Goodbye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(n);
+            }
+        }
+        reader.close();
     }
 }
+

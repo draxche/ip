b66cb36f2b2ae11a7638def215e3b3c5b3b7f5dc
Kaile <kailechong060@gmail.com>
2026-08-28 14:37:42 +0800

    Add unit tests for Parser and ScheduleDateTime

---
+++ src/test/java/drax/ParserTest.java
@@ -0,0 +1,109 @@
+package drax;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void parse_byeCommand_returnsByeCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.BYE, "", "", "", "");
+        Parser.Command actual = Parser.parse("bye");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_todoCommand_returnsTodoCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.TODO, "", "homework", "", "");
+        Parser.Command actual = Parser.parse("todo homework");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_deadlineCommand_returnsDeadlineCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.DEADLINE, "", "return book", "06/06/2026 1800", "");
+        Parser.Command actual = Parser.parse("deadline return book /by 06/06/2026 1800");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_deadlineWithoutDate_returnsTaskWithEmptyDate() {
+        Parser.Command expected =
+                new Parser.Command(
+                        Parser.Type.DEADLINE,
+                        "",
+                        "return book",
+                        "",
+                        ""
+                );
+        Parser.Command actual = Parser.parse("deadline return book");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_eventCommand_returnsEventCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.EVENT,
+                "",
+                "meeting",
+                "20/08/2026 1200",
+                "20/08/2026 1400");
+        Parser.Command actual = Parser.parse("event meeting /from 20/08/2026 1200 /to 20/08/2026 1400");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_eventWithoutDates_returnsTaskWithEmptyDates() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.EVENT,
+                "",
+                "meeting",
+                "",
+                ""
+        );
+        Parser.Command actual = Parser.parse("event meeting");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_unknownCommand_returnsUnknownCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.UNKNOWN, "blah", "", "", "");
+        Parser.Command actual = Parser.parse("blah");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_listCommand_returnsListCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.LIST, "", "", "", "");
+        Parser.Command actual = Parser.parse("list");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_unmarkCommand_returnsUnmarkCommandWithIndex() {
+        Parser.Command expected = new Parser.Command(Parser.Type.UNMARK, "2", "", "", "");
+        Parser.Command actual = Parser.parse("unmark 2");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_markCommand_returnsMarkCommandWithIndex() {
+        Parser.Command expected = new Parser.Command(Parser.Type.MARK, "3", "", "", "");
+        Parser.Command actual = Parser.parse("mark 3");
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void parse_deleteCommand_returnsDeleteCommandWithIndex() {
+        Parser.Command expected = new Parser.Command(Parser.Type.DELETE, "5", "", "", "");
+        Parser.Command actual = Parser.parse("delete 5");
+        assertEquals(expected, actual);
+    }
+}
+
+
---
+++ src/test/java/drax/ScheduleDateTimeTest.java
@@ -0,0 +1,64 @@
+package drax;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ScheduleDateTimeTest {
+    @Test
+    public void parse_isoDate_returnsDateAtMidnight() {
+        LocalDateTime expected = LocalDateTime.of(2026, 12, 2, 0, 0);
+        LocalDateTime actual = ScheduleDateTime.parse("2026-12-02");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_isoDateTime_returnsDateAndTime() {
+        LocalDateTime expected = LocalDateTime.of(2026, 12, 2, 18, 30);
+        LocalDateTime actual = ScheduleDateTime.parse("2026-12-02T18:30");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void parse_commandDateAndTime_returnsDateAndTime() {
+        LocalDateTime expected = LocalDateTime.of(2026, 12, 2, 18, 30);
+        LocalDateTime actual = ScheduleDateTime.parse("02/12/2026 1830");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_invalidDateFormat_exceptionThrown() {
+        try {
+            ScheduleDateTime.parse("2026-12-02 1830");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Please use a valid date and time: "
+                    + "yyyy-MM-dd, yyyy-MM-ddTHH:mm, or d/M/yyyy HHmm.", e.getMessage());
+        }
+    }
+    @Test
+    public void parse_invalidDate_exceptionThrown() {
+        try {
+            ScheduleDateTime.parse("31/02/2026 1830");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Please use a valid date and time: "
+                    + "yyyy-MM-dd, yyyy-MM-ddTHH:mm, or d/M/yyyy HHmm.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void format_midnight_returnsDateOnly() {
+        LocalDateTime dateTime = LocalDateTime.of(2026, 12, 2, 0, 0);
+        String actual = ScheduleDateTime.format(dateTime);
+        assertEquals("Dec 02 2026", actual);
+    }
+
+    @Test
+    public void format_nonMidnight_returnsDateAndTime() {
+        LocalDateTime dateTime = LocalDateTime.of(2026, 12, 2, 18, 30);
+        String actual = ScheduleDateTime.format(dateTime);
+        assertEquals("Dec 02 2026 6:30 PM", actual);
+    }
+
+}

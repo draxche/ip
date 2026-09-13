2822ebf38475603d1619ebcd047021301e0e3c02
Kaile <kailechong060@gmail.com>
2026-09-13 22:02:56 +0800

    Update TaskMemento with copy behaviour

--- src/main/java/drax/TaskMemento.java
+++ src/main/java/drax/TaskMemento.java
@@ -1,20 +1,24 @@
 package drax;
 
 /**
- * Stores an immutable current snapshot of the TaskList
+ * Stores a current snapshot of the TaskList
  */
 public class TaskMemento {
     private final TaskList taskList;
 
+    /**
+     * Creates a new TaskMemento
+     * @param taskList the TaskList to be copied as a snapshot
+     */
     public TaskMemento(TaskList taskList) {
-        this.taskList = taskList;
+        this.taskList = new TaskList(taskList.asList());
     }
 
     /**
      * Extracts the saved TaskList content from the memento
-     * @return TaskList the saved TaskList content
+     * @return TaskList a new TaskList containing the same field as the stored snapshot
      */
     public TaskList getSavedContent() {
-        return this.taskList;
+        return new TaskList(this.taskList.asList());
     }
 }

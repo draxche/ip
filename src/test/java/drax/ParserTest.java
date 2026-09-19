72d48506a925329fb931f643c3720a4ef6d1e015
Kaile <kailechong060@gmail.com>
2026-09-19 15:04:06 +0800

    Add unit tests for parser parse method

--- src/test/java/drax/ParserTest.java
+++ src/test/java/drax/ParserTest.java
@@ -2,6 +2,8 @@ package drax;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class ParserTest {
@@ -19,6 +21,22 @@ public class ParserTest {
                 Parser.Type.TODO, "", "homework", "", "");
         Parser.Command actual = Parser.parse("todo homework");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_bareTodo_returnsTodoWithEmptyDescription() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.TODO, "", "", "", "");
+
+        assertEquals(expected, Parser.parse("todo"));
+    }
+
+    @Test
+    public void parse_todoWithExtraWhitespace_trimsDescription() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.TODO, "", "finish homework", "", "");
+
+        assertEquals(expected, Parser.parse("todo    finish homework   "));
     }
 
     @Test
@@ -57,6 +75,38 @@ public class ParserTest {
                 Parser.Type.DEADLINE, "", "", "", "2026-09-11");
 
         assertEquals(expected, Parser.parse("deadline /by 2026-09-11"));
+    }
+
+    @Test
+    public void parse_deadlineWithEmptyDate_returnsEmptyDate() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.DEADLINE, "", "return book", "", "");
+
+        assertEquals(expected, Parser.parse("deadline return book /by "));
+    }
+
+    @Test
+    public void parse_deadlineWithExtraWhitespace_trimsTaskAndDate() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.DEADLINE, "", "return book", "", "2026-09-11");
+
+        assertEquals(expected, Parser.parse("deadline    return book    /by    2026-09-11   "));
+    }
+
+    @Test
+    public void parse_deadlineWithUnspacedMarker_treatsMarkerAsDescription() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.DEADLINE, "", "return book/by 2026-09-11", "", "");
+
+        assertEquals(expected, Parser.parse("deadline return book/by 2026-09-11"));
+    }
+
+    @Test
+    public void parse_deadlineWithSecondByMarker_preservesItInDate() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.DEADLINE, "", "return book", "", "2026-09-11 /by evening");
+
+        assertEquals(expected, Parser.parse("deadline return book /by 2026-09-11 /by evening"));
     }
 
     @Test
@@ -128,11 +178,99 @@ public class ParserTest {
     }
 
     @Test
+    public void parse_eventWithWhitespaceEndDate_returnsEmptyEndDate() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.EVENT, "", "meeting", "2026-09-11", "");
+
+        assertEquals(expected, Parser.parse("event meeting /from 2026-09-11 /to "));
+    }
+
+    @Test
+    public void parse_eventWithOnlyFromMarker_treatsMarkerAsDescription() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.EVENT, "", "meeting /from 2026-09-11", "", "");
+
+        assertEquals(expected, Parser.parse("event meeting /from 2026-09-11"));
+    }
+
+    @Test
+    public void parse_eventWithOnlyToMarker_treatsMarkerAsDescription() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.EVENT, "", "meeting /to 2026-09-12", "", "");
+
+        assertEquals(expected, Parser.parse("event meeting /to 2026-09-12"));
+    }
+
+    @Test
+    public void parse_eventWithExtraWhitespace_trimsAllExtractedFields() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.EVENT, "", "meeting", "2026-09-11", "2026-09-12");
+
+        assertEquals(expected,
+                Parser.parse("event    meeting    /from    2026-09-11    /to    2026-09-12   "));
+    }
+
+    @Test
+    public void parse_eventWithSecondToMarker_preservesItInEndDate() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.EVENT, "", "meeting", "2026-09-11", "2026-09-12 /to evening");
+
+        assertEquals(expected,
+                Parser.parse("event meeting /from 2026-09-11 /to 2026-09-12 /to evening"));
+    }
+
+    @Test
     public void parse_unknownCommand_returnsUnknownCommand() {
         Parser.Command expected = new Parser.Command(
                 Parser.Type.UNKNOWN, "blah", "", "", "");
         Parser.Command actual = Parser.parse("blah");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_emptyInput_returnsUnknownCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.UNKNOWN, "", "", "", "");
+
+        assertEquals(expected, Parser.parse(""));
+    }
+
+    @Test
+    public void parse_unknownCommandWithWhitespace_preservesOriginalInput() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.UNKNOWN, "  not a command  ", "", "", "");
+
+        assertEquals(expected, Parser.parse("  not a command  "));
+    }
+
+    @Test
+    public void parse_commandWithLeadingWhitespace_returnsUnknownCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.UNKNOWN, " todo homework", "", "", "");
+
+        assertEquals(expected, Parser.parse(" todo homework"));
+    }
+
+    @Test
+    public void parse_uppercaseCommand_returnsUnknownCommand() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.UNKNOWN, "TODO homework", "", "", "");
+
+        assertEquals(expected, Parser.parse("TODO homework"));
+    }
+
+    @Test
+    public void parse_commandsWithExtraSuffix_returnsUnknownCommands() {
+        List<String> inputs = List.of(
+                "bye now", "listing", "marker 1", "unmarked 1", "todoing homework",
+                "deadlines task", "eventful meeting", "deleted 1", "finder book",
+                "undoing", "redoing");
+
+        for (String input : inputs) {
+            Parser.Command actual = Parser.parse(input);
+            assertEquals(Parser.Type.UNKNOWN, actual.type(), input + " should be unknown");
+            assertEquals(input, actual.argument(), input + " should be preserved");
+        }
     }
 
     @Test
@@ -153,11 +291,43 @@ public class ParserTest {
     }
 
     @Test
+    public void parse_bareUnmark_returnsUnmarkCommandWithEmptyIndex() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.UNMARK, "", "", "", "");
+
+        assertEquals(expected, Parser.parse("unmark"));
+    }
+
+    @Test
+    public void parse_unmarkWithExtraWhitespace_trimsIndex() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.UNMARK, "2", "", "", "");
+
+        assertEquals(expected, Parser.parse("unmark    2   "));
+    }
+
+    @Test
     public void parse_markCommand_returnsMarkCommandWithIndex() {
         Parser.Command expected = new Parser.Command(
                 Parser.Type.MARK, "3", "", "", "");
         Parser.Command actual = Parser.parse("mark 3");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_bareMark_returnsMarkCommandWithEmptyIndex() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.MARK, "", "", "", "");
+
+        assertEquals(expected, Parser.parse("mark"));
+    }
+
+    @Test
+    public void parse_markWithExtraWhitespace_trimsIndex() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.MARK, "3", "", "", "");
+
+        assertEquals(expected, Parser.parse("mark    3   "));
     }
 
 
@@ -170,10 +340,43 @@ public class ParserTest {
     }
 
     @Test
+    public void parse_bareDelete_returnsDeleteCommandWithEmptyIndex() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.DELETE, "", "", "", "");
+
+        assertEquals(expected, Parser.parse("delete"));
+    }
+
+    @Test
+    public void parse_deleteWithExtraWhitespace_trimsIndex() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.DELETE, "5", "", "", "");
+
+        assertEquals(expected, Parser.parse("delete    5   "));
+    }
+
+    @Test
     public void parse_findCommand_returnsFindCommandWithKeyword() {
-        Parser.Command expected = new Parser.Command(Parser.Type.FIND, "clean house", "", "", "");
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.FIND, "clean house", "", "", "");
         Parser.Command actual = Parser.parse("find clean house");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parse_bareFind_returnsFindCommandWithEmptyKeyword() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.FIND, "", "", "", "");
+
+        assertEquals(expected, Parser.parse("find"));
+    }
+
+    @Test
+    public void parse_findWithExtraWhitespace_trimsKeyword() {
+        Parser.Command expected = new Parser.Command(
+                Parser.Type.FIND, "clean house", "", "", "");
+
+        assertEquals(expected, Parser.parse("find    clean house   "));
     }
 
     @Test
